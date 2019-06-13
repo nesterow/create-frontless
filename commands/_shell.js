a4b6575e15616c674ff78518c0eedddb1c02e2d4
Anton Nesterov <arch.nesterov@gmail.com>
2019-06-13 15:14:32 +0300

    [clean up]

--- commands/_shell.js
+++ commands/_shell.js
@@ -1,26 +1,5 @@
 const { spawn } = require('child_process');
 
-
-// const repoURL = 'https://github.com/nesterow/frontless';
-
-// runCommand('git', ['clone', repoURL, name])
-//   .then(() => {
-//     return runCommand('rm', ['-rf', `${name}/.git`]);
-//   }).then(() => {
-//     console.log('Installing dependencies...');
-//     return runCommand('npm', ['install'], {
-//       cwd: process.cwd() + '/' + name
-//     });
-//   }).then(() => {
-//     console.log('Done! 🏁');
-//     console.log('');
-//     console.log('To get started:');
-//     console.log('cd', name);
-//     console.log('yarn start');
-//     console.log('- or -');
-//     console.log('npm start');
-//   });
-
 module.exports = function runCommand(command, args, options = undefined) {
   const spawned = spawn(command, args, options);
 
